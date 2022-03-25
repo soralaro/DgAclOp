e06ceb482c1291c1f0447c5d1ef89f57fb2a94a4
zhenxiongchen <zhenxiongchen@deepglint.com>
2022-03-26 00:31:27 +0800

    fix:memset 0 edge

--- cpukernel/impl/vega_crop_resize_kernels.cc
+++ cpukernel/impl/vega_crop_resize_kernels.cc
@@ -31,33 +31,50 @@ uint32_t VegaCropResizeCpuKernel::Compute(CpuKernelContext &ctx)
 
     unsigned char *img_in = (unsigned char *) param->in.addr;
     if(param->in.type == (unsigned int )NV12){
-        cv::Mat bgr;
         bool to_resize=true;
+        bool to_copy=false;
+ 
         int img_in_w=param->in.w;
         if(img_in_w %2 !=0 ){
             img_in_w--;
         }
+        cv::Rect r=cv::Rect(param->in.roi_x,param->in.roi_y,param->in.roi_w,param->in.roi_h);
+        auto tl = r.tl();
+        auto br = r.br();
+        if(tl.x % 2 != 0) tl.x++;
+        if(tl.y % 2 != 0) tl.y++;
+        if(br.x % 2 != 0) br.x--;
+        if(br.y % 2 != 0) br.y--;
+        cv::Rect newRoi(tl, br);
+
+        cv::Mat bgr;
         if(param->in.w==param->out.w&&param->in.h==param->out.h&&
             param->in.roi_x==param->out.roi_x&&param->in.roi_y==param->out.roi_y&&
-            param->in.roi_w==param->out.roi_w&&param->in.roi_h==param->out.roi_h){
-            auto mat=cv::Mat(param->out.h,param->out.w,CV_8UC3,(uchar*)param->out.addr,param->out.s_w);
-            bgr=mat(cv::Rect(0,0,img_in_w,param->out.h/2*2));
+            param->in.roi_w==param->out.roi_w&&param->in.roi_h==param->out.roi_h&&
+            param->out.w==param->out.roi_w&&param->out.roi_x==0&&param->out.roi_y==0){
+            bgr=cv::Mat(param->out.h,param->out.w,CV_8UC3,(uchar*)param->out.addr,param->out.s_w);
             to_resize=false;
-        }else{
+        }else if(param->in.roi_w==param->out.roi_w&&param->in.roi_h==param->out.roi_h){
+            to_resize=false;
+            to_copy=true;
+        }
+        if(to_resize||to_copy){
             bgr=cv::Mat(param->in.h/2*2,img_in_w,CV_8UC3);
         }
+        bgr=cv::Mat(param->in.h/2*2,img_in_w,CV_8UC3);
         cv::Size sz(img_in_w,param->in.h/2*3);
         cv::Mat nv12=cv::Mat(sz, CV_8UC1, (uchar*)param->in.addr,param->in.s_w);
+        if(param->out.w*param->out.h > param->out.roi_w*param->out.roi_h){
+            memset((uchar*)param->out.addr,0,param->out.s_w*param->out.s_h);
+        }
         cv::cvtColor(nv12, bgr,cv::COLOR_YUV2BGR_NV12);
+        if(to_copy) {
+            auto dst=cv::Mat(param->out.h,param->out.w,CV_8UC3,(uchar*)param->out.addr,param->out.s_w);
+            auto dst_roi=dst(newRoi);
+            cv::Mat img_in_roi= bgr(newRoi);
+            img_in_roi.copyTo(dst_roi);
+        }
         if(to_resize){
-            cv::Rect r=cv::Rect(param->in.roi_x,param->in.roi_y,param->in.roi_w,param->in.roi_h);
-            auto tl = r.tl();
-            auto br = r.br();
-            if(tl.x % 2 != 0) tl.x++;
-            if(tl.y % 2 != 0) tl.y++;
-            if(br.x % 2 != 0) br.x--;
-            if(br.y % 2 != 0) br.y--;
-            cv::Rect newRoi(tl, br);
             cv::Mat img_in_roi= bgr(newRoi);
             cv::Mat img_out = cv::Mat(param->out.h, param->out.w,CV_8UC3,(uchar*)param->out.addr,param->out.s_w);
             cv::Mat img_out_roi= img_out(cv::Rect(param->out.roi_x,param->out.roi_y,param->out.roi_w,param->out.roi_h));
